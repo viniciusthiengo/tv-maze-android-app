64c38957131378d6402f3c32792e590ea08bf217
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-10-01 23:41:27 -0300

    Adding the first BE API use.

--- app/src/main/java/thiengo/com/br/tvmaze/MainActivity.kt
+++ app/src/main/java/thiengo/com/br/tvmaze/MainActivity.kt
@@ -1,11 +1,34 @@
 package thiengo.com.br.tvmaze
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import thiengo.com.br.tvmaze.network.FetchShows
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        FetchShows
+            .Builder(this)
+            .setOnLoadingCallback {
+                Log.d("thiengo", "OnLoadingCallback")
+            }
+            .setOnSuccessCallback {
+                Log.d("thiengo", "OnSuccessCallback")
+
+                it.forEach {
+                    Log.d("thiengo", "Show name: ${it?.name}")
+                }
+            }
+            .setOnFailureCallback {
+                Log.d("thiengo", "OnFailureCallback")
+            }
+            .build()
+            .search(query = "girls")
+
     }
 }
